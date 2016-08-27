135423ae38a110a6dee4ee696a74e1670c72ab53
amosbaby <931963081@qq.com>
2016-08-27 14:56:16 +0800

    Update UIView+RoundCorner.h
    更改设置圆角的注释

--- UIView+RoundCorner.h
+++ UIView+RoundCorner.h
@@ -1,6 +1,6 @@
 //
 //  UIImageView+RoundCorner.h
-//  loxer
+//  
 //
 //  Created by amos on 16/5/20.
 //  Copyright © 2016年 zoneol. All rights reserved.
@@ -11,9 +11,11 @@
 @interface UIView (RoundCorner)
 
 /**
- *  设置图片为原型
+ *  设置的圆角--默认半径与高度相同
  */
 -(void) setRoundCorner;
-
+/**
+ *  设置的圆角--需手动设置圆角半径
+ */
 -(void)setRoundCorner:(CGFloat) cornerRadius;
 @end
